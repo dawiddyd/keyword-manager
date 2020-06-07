d0f3d75d11a6eb493292b0577f30e726735f8bc0
Dawid Dydliński <dawiddydlinski@gmail.com>
2020-06-07 22:12:37 +0200

    Update module providers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { CategoryService } from './services/category/category.service';
+import { KeywordService } from './services/keyword/keyword.service';
+import { KeywordsResolver } from './keywords.resolver';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
   }),],
-  controllers: [],
-  providers: [],
+  providers: [KeywordService, CategoryService, KeywordsResolver],
 })
 export class AppModule {}
