912dbf62cec6c7890cbc798c63839ae1720a6cfe
Dawid Dydliński <dawiddydlinski@gmail.com>
2020-06-07 19:46:10 +0200

    Add basic methods to manipulate data in category service

--- backend/src/services/category/category.service.ts
+++ backend/src/services/category/category.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { data } from '../../mock';
+import { Category } from '../../category.model';
+import { Keyword } from '../../keyword.model';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  data = data;
+
+  getCategory(id: number): Category {
+    return this.data.find(c => c.id == id);
+  }
+
+  createCategory(name: string): Category {
+    const id = this.data.length;
+    const newCategory = { id: id, name, keywords: [] };
+    this.data.push(newCategory);
+    return this.data.find(c => c.name == name);
+  }
+
+  deleteCategory(id: number): boolean {
+    this.data = this.data.filter(c => c.id != id);
+    return true;
+  }
+}
