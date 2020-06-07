d227e1dc2280d7b3b36f916bfee42994b3909876
Dawid Dydliński <dawiddydlinski@gmail.com>
2020-06-07 19:09:10 +0200

    Import graphql module with options object

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }),],
   controllers: [],
   providers: [],
 })
