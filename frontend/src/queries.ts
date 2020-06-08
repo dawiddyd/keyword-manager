817fba198770769e44eabd98596f36e779a27664
Dawid Dydliński <dawiddydlinski@gmail.com>
2020-06-08 18:04:16 +0200

    Create queries file

---
+++ frontend/src/queries.ts
@@ -0,0 +1,14 @@
+import {gql} from "@apollo/client";
+
+export const GET_CATEGORIES = gql`
+    {
+        categories {
+            id
+            name
+            keywords {
+                id
+                name
+            }
+        }
+    }
+`;
