274b9e79904f44ad0064a4d2ad99506b1dda6c1a
Dawid Dydliński <dawiddydlinski@gmail.com>
2020-06-11 23:33:57 +0200

    Add logic to fetch keywords from extrenal service

--- backend/src/services/category/category.service.ts
+++ backend/src/services/category/category.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { data } from '../../mock';
 import { Category } from '../../category.model';
+import fetch from 'node-fetch';
 
 @Injectable()
 export class CategoryService {
@@ -14,9 +15,19 @@ export class CategoryService {
     return category;
   }
 
-  createCategory(name: string): Category {
+  async createCategory(name: string): Promise<Category> {
+    // Creating new category with incremental id. Pre-filling with 10 keywords from server.
     const id = Math.max(0, ...this.data.map(c => c.id)) + 1;
     const newCategory = { id: id, name, keywords: [] };
+    try {
+      let res = await fetch(`https://api.datamuse.com/words?ml=${name}&max=10`);
+      let words = await res.json();
+      words.map(((w, i) => {
+        newCategory.keywords.push({ id: i, name: w.word });
+      }));
+    } catch (e) {
+      throw new InternalServerErrorException(e);
+    }
     this.data.push(newCategory);
     return newCategory;
   }
