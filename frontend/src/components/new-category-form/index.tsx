3eeb0b9a089bf8e15fc53a59521ce311f65f7437
Dawid Dydliński <dawiddydlinski@gmail.com>
2020-06-11 21:32:25 +0200

    Add props object to new-category-form component

--- frontend/src/components/new-category-form/index.tsx
+++ frontend/src/components/new-category-form/index.tsx
@@ -4,7 +4,11 @@ import {CREATE_CATEGORY, GET_CATEGORIES} from "../../queries";
 import {toast} from "react-toastify";
 import {Category} from "../../models/category.model";
 
-const _NewCategoryForm = () => {
+interface OwnProps {
+    className?: string;
+}
+
+const _NewCategoryForm = (props: OwnProps) => {
     const [categoryName, setCategoryName] = useState();
     const [buttonVisible, setButtonVisible] = useState(false);
     const [createCategory] = useMutation(CREATE_CATEGORY, {
@@ -24,13 +28,13 @@ const _NewCategoryForm = () => {
     });
 
     return (
-        <div>
-            <button onClick={() => setButtonVisible(!buttonVisible)}>New Category</button>
-            {buttonVisible && <input onChange={(event) => setCategoryName(event.target.value)} type="text"/>}
-            {buttonVisible && <button onClick={async () => {
+        <div className="new-category-form">
+            {buttonVisible && <input className="new-category-form__name-input" placeholder="Category name" onChange={(event) => setCategoryName(event.target.value)} type="text"/>}
+            {buttonVisible && <button className="new-category-form__button" onClick={async () => {
                 await createCategory();
                 setButtonVisible(false)
             }}>Save</button>}
+            <button className="new-category-form__button" onClick={() => setButtonVisible(!buttonVisible)}>+ Add Category</button>
         </div>
     );
 }
